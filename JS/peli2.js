3cf6720eca463a588a4f60fd61853da45c5b7600
jooksane24 <jooksane24@students.oamk.fi>
2025-11-29 10:44:40 +0200

    Kuvien vaihto

--- JS/peli2.js
+++ JS/peli2.js
@@ -1,9 +1,10 @@
-const questions = ['Question one',' Question two','Question three',' Question four', 'Question five' ];
+const questions = ['Taso1','Taso2','Taso3','Taso4', 'Taso5' ];
 const answers = ['b','c','a','b','a'];
-const index = 0;
+let index = 0;
 
 const images = document.getElementsByTagName('img');
 const questionHeader = document.getElementById('question');
+const feedback = document.getElementById('feedback');
 
 images[0].addEventListener('click', imageClicked);
 images[1].addEventListener('click', imageClicked);
@@ -14,14 +15,33 @@ setQuestion();
 
 function setImages(){
     images[0].src = './Images/Peli2/'+ (index+1) +'a.jpg';
+    images[0].dataset.answer = 'a';
     images[1].src = './Images/Peli2/'+ (index+1) +'b.jpg';
+    images[1].dataset.answer = 'b';
     images[2].src = './Images/Peli2/'+ (index+1) +'c.jpg';
+    images[2].dataset.answer = 'c';
 
 }
 function setQuestion(){
     questionHeader.textContent = questions[index];
+    feedback.textContent = ''; // tyhjennetään palaute uuden kysymyksen alussa
 }
 
-function imageClicked(){
+function imageClicked(event){
+    if(event.currentTarget.dataset.answer === answers[index]){
+        feedback.textContent = 'Oikea vastaus!';
+        feedback.style.color = 'green';
+    } else {
+        feedback.textContent = 'Väärä vastaus!';
+        feedback.style.color = 'red';
+    }
 
+    index++;
+    if(index < questions.length){
+        setImages();
+        setQuestion();
+    } else {
+        feedback.textContent = 'Peli loppui!';
+        feedback.style.color = 'blue';
+    }
 }
